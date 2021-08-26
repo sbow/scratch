99bf79f546c9001b4258088be94007ad0b00b761
shaun bowman <shaun_bowman@hotmail.com>
2021-08-25 23:54:32 -0400

    Implemented AV throt up down request, and tele throttle steer request, in gamepad logic

--- myCatkin/src/teleop/src/teleop_logi.cpp
+++ myCatkin/src/teleop/src/teleop_logi.cpp
@@ -2,6 +2,8 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Bool.h>
+#include <std_msgs/Int8.h>
+#include <std_msgs/Float32.h>
 #include <sensor_msgs/Joy.h>
 #include <vector>
 #include <sstream>
@@ -30,7 +32,13 @@ bool av_throtup_b = false;
 bool av_throtdn_b = false;
 bool ros_start_b = false;
 bool ros_stop_b = false;
+int enum_AV_THROT_NC;
+int enum_AV_THROT_INC;
+int enum_AV_THROT_DEC;
 
+// Global state vars
+
+// Set global vars upon publication of new Joy topic
 void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
 {
     std::vector<int32_t> button = msg->buttons;
@@ -44,7 +52,7 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
     ros_start_b = button[ros_start_btn] == 1;
     ros_stop_b = button[ros_stop_btn] == 1;
     tele_steer_flt = axes[tele_steer_ax];
-    tele_throt_ax = axes[tele_throt_ax];
+    tele_throt_flt = axes[tele_throt_ax];
     ROS_INFO("tele_dedman_btn val: %d", tele_dedman_b); // debug
 }
 
@@ -54,38 +62,79 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
   ros::Rate loop_rate(50);
 
-// Read in button assignments from params.yaml
+  // Read in button assignments from params.yaml
 
   bool gotParam = true; 
 
-  gotParam = n.getParam("/teleop_logi/tele_dedman_btn", tele_dedman_btn); 
-  gotParam = n.getParam("/teleop_logi/tele_steer_ax", tele_steer_ax); 
-  gotParam = n.getParam("/teleop_logi/tele_throt_ax", tele_throt_ax); 
-  gotParam = n.getParam("/teleop_logi/av_dedman_btn", av_dedman_btn); 
-  gotParam = n.getParam("/teleop_logi/av_start_btn", av_start_btn); 
-  gotParam = n.getParam("/teleop_logi/av_pause_btn", av_pause_btn); 
-  gotParam = n.getParam("/teleop_logi/av_throtup_btn", av_throtup_btn); 
-  gotParam = n.getParam("/teleop_logi/av_throtdn_btn", av_throtdn_btn); 
-  gotParam = n.getParam("/teleop_logi/ros_start_btn", ros_start_btn); 
-  gotParam = n.getParam("/teleop_logi/ros_stop_btn", ros_stop_btn); 
+  gotParam = gotParam && n.getParam("/teleop_logi/tele_dedman_btn", tele_dedman_btn); 
+  gotParam = gotParam && n.getParam("/teleop_logi/tele_steer_ax", tele_steer_ax); 
+  gotParam = gotParam && n.getParam("/teleop_logi/tele_throt_ax", tele_throt_ax); 
+  gotParam = gotParam && n.getParam("/teleop_logi/av_dedman_btn", av_dedman_btn); 
+  gotParam = gotParam && n.getParam("/teleop_logi/av_start_btn", av_start_btn); 
+  gotParam = gotParam && n.getParam("/teleop_logi/av_pause_btn", av_pause_btn); 
+  gotParam = gotParam && n.getParam("/teleop_logi/av_throtup_btn", av_throtup_btn); 
+  gotParam = gotParam && n.getParam("/teleop_logi/av_throtdn_btn", av_throtdn_btn); 
+  gotParam = gotParam && n.getParam("/teleop_logi/ros_start_btn", ros_start_btn); 
+  gotParam = gotParam && n.getParam("/teleop_logi/ros_stop_btn", ros_stop_btn); 
   
   if(not gotParam)
   {
       ROS_FATAL("Didnt get tele_xxxx_yyy param");
   };
 
-// Subscribe to the ROS Driver node - joy - to get raw gamepad button presses
+  // Subscribe to the ROS Driver node - joy - to get raw gamepad button presses
   ros::Subscriber sub = n.subscribe("joy", 1000, joyCallback);
 
-// Publish teleop state for use in axl_trq_arb node
-  ros::Publisher pub = n.advertise<std_msgs::Bool>("/teleop_logi/dedman_depressed",5);
-  std_msgs::Bool dedman_depressed;
+  // Publish teleop state for use in axl_trq_arb node
+  ros::Publisher pub_hil = n.advertise<std_msgs::Bool>("/teleop_logi/human_in_loop",5);
+  std_msgs::Bool human_in_loop;
+  gotParam = true; // reset gotParam tracking boolean
+  gotParam = gotParam && n.getParam("/teleop_logi/AV_THROT_NC",enum_AV_THROT_NC);
+  gotParam = gotParam && n.getParam("/teleop_logi/AV_THROT_INC",enum_AV_THROT_INC);
+  gotParam = gotParam && n.getParam("/teleop_logi/AV_THROT_DEC",enum_AV_THROT_DEC);
+  if(not gotParam)
+    ROS_FATAL("Didn't get AV_THROT_XX enum param");
+
+  // Publish AV throttle request 
+  ros::Publisher pub_avthrot = n.advertise<std_msgs::Int8>("/teleop_logi/av_throt_req",5);
+  std_msgs::Int8 av_throt_req;
+
+  // Publish teleop throttle and steering
+  ros::Publisher pub_telesteer = n.advertise<std_msgs::Float32>("/teleop_logi/tele_steer_req",5);
+  std_msgs::Float32 tele_steer_req;
+  ros::Publisher pub_telethrot = n.advertise<std_msgs::Float32>("/teleop_logi/tele_throt_req",5);
+  std_msgs::Float32 tele_throt_req;
+
+  // Publish ROS Bag record request
 
   while (ros::ok())
   {
     // process dedman switch
-    dedman_depressed.data = tele_dedman_b || av_dedman_b;
-    pub.publish(dedman_depressed);
+    human_in_loop.data = tele_dedman_b || av_dedman_b;
+    pub_hil.publish(human_in_loop);
+
+    // process AV throttle request
+    if(av_throtup_b && av_dedman_b)
+        av_throt_req.data = enum_AV_THROT_INC;
+    else if(av_throtdn_b && av_dedman_b)
+        av_throt_req.data = enum_AV_THROT_DEC;
+    else
+        av_throt_req.data = enum_AV_THROT_NC;
+    pub_avthrot.publish(av_throt_req);
+
+    // process teleop throttle and steering
+    if(tele_dedman_b)
+    {
+        tele_steer_req.data = tele_steer_flt;
+        tele_throt_req.data = tele_throt_flt;
+    }
+    else
+    {
+        tele_steer_req.data = 0.0;
+        tele_throt_req.data = 0.5;
+    }
+    pub_telesteer.publish(tele_steer_req); // note: raw values 0..1; must be converted to
+    pub_telethrot.publish(tele_throt_req); // physical units in axl_trq_arb
 
     ros::spinOnce();
     loop_rate.sleep();
